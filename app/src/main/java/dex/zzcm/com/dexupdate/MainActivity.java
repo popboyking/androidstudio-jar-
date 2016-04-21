cb75d5e2f14bba80c1137e0b4c68a397dbf27fad
popboyking <popboyking@gmail.com>
2016-04-21 13:59:04 +0800

    动态热修补代码提交

--- app/src/main/java/dex/zzcm/com/dexupdate/MainActivity.java
+++ app/src/main/java/dex/zzcm/com/dexupdate/MainActivity.java
@@ -1,21 +1,79 @@
 package dex.zzcm.com.dexupdate;
 
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+
+import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
 
+import dex.zzcm.com.dexupdate.core.DexManager;
+import dex.zzcm.com.dexupdate.core.Utils;
+import dex.zzcm.com.dexupdate.net.DownFile;
+import dodola.hotfixlib.HotFix;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    Button btn_fix,btn_getFix;
+    TextView txt_source;
+    SourceSystemManager ssm ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
+        ssm = new SourceSystemManager();
+        this.setContentView(R.layout.activity_main);
+
+        txt_source = (TextView) findViewById(R.id.txt_source);
+        txt_source.setText("修复前:"+ssm.getSource());
+        btn_fix = (Button) findViewById(R.id.fix);
+
+        btn_fix.setOnClickListener(this);
+
+        btn_getFix = (Button) findViewById(R.id.getfixJar);
+        btn_getFix.setOnClickListener(this);
+
+
+        Toast.makeText(this,SourceSystemManager.printName(),Toast.LENGTH_LONG).show();
+
+
     }
 
+
+    @Override
+    public void onClick(View view) {
+        if(view == btn_fix){
+            txt_source.setText("修复后:"+ssm.getSource());
+            Toast.makeText(this,SourceSystemManager.printName(),Toast.LENGTH_LONG).show();
+            restartApp();
+        }
+
+        if(view == btn_getFix){
+            String url = "http://192.168.0.48:8080/examples/pathDex.jar";//自己随便搭两个tomcat将就用下
+            DownFile downFile = new DownFile();
+            downFile.downFile(new DownFile.IFileOK() {
+                @Override
+                public void success(File file) {
+
+                    Log.e("down","下载成功:"+file.getAbsolutePath());
+                    //加载修复文件
+
+                }
+            },url);
+        }
+    }
+
+    public void restartApp(){
+        Intent intent = getPackageManager().getLaunchIntentForPackage(getPackageName());
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+    }
 
 }
---
+++ app/src/main/java/dex/zzcm/com/dexupdate/core/DexManager.java
@@ -0,0 +1,55 @@
+package dex.zzcm.com.dexupdate.core;
+
+import android.content.Context;
+import android.util.Log;
+
+import java.io.File;
+
+import dodola.hotfixlib.HotFix;
+
+/**
+ * Created by zhu on 2016/4/1.
+ */
+public class DexManager
+{
+
+    public static final String DEX_PATH = "tempDex.jar";
+    public static final String TEMP_COM = "dex.zzcm.com.codefix.CombomBox";//通过此类避免被打上 CLASS_ISPREVERIFIED
+    public static final String USER = "dex.zzcm.com.pathmodule.User";
+
+
+
+    public void getName(){
+        Log.i("H",DexManager.class.getSimpleName()+"--->getName()" );
+    }
+
+    /**
+     * 加载dex file 文件
+     */
+    public static void  loadDex(Context context){
+        File dexfile = new File(context.getDir("dir",Context.MODE_PRIVATE),DEX_PATH);
+        Utils.prepareDex(context,dexfile,null,true);
+        HotFix.patch(context,dexfile.getAbsolutePath(),TEMP_COM);
+
+        try {
+            context.getClassLoader().loadClass(TEMP_COM);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        File f = new File ("/storage/emulated/0/updateDex/path.jar");
+        if(f.exists()){
+            loadDex(context,f,USER);
+        }
+    }
+
+    public static void loadDex(Context context,File fromFile,String classFullName){
+
+        File dexfile = new File(context.getDir("dir",Context.MODE_PRIVATE),fromFile.getName());
+        Utils.prepareDex(context,dexfile,fromFile,false);
+        HotFix.patch(context,dexfile.getAbsolutePath(),classFullName);
+
+    }
+
+
+}
